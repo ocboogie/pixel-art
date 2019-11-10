d3a63872e3a6b876272020ab90706a513cc741a1
ocboogie <boogie@mikulec.com>
2019-11-09 20:12:47 -0800

    Add a Authenticated middleware

--- api/authenticating.go
+++ api/authenticating.go
@@ -8,7 +8,23 @@ import (
 	"github.com/ocboogie/pixel-art/models"
 )
 
-const SessionIDCookieName = "sessionId"
+const sessionIDCookieName = "session_id"
+
+func (s *server) Authenticated(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		cookie, err := c.Cookie(sessionIDCookieName)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusUnauthorized, "You must login")
+		}
+		userID, err := s.authenticating.VerifySession(cookie.Value)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusUnauthorized, "Invalid Session")
+		}
+		c.Set("userID", userID)
+
+		return next(c)
+	}
+}
 
 func (s *server) handlerSignUp(c echo.Context) error {
 	userInput := new(models.UserInput)
@@ -48,7 +64,7 @@ func (s *server) handlerLogin(c echo.Context) error {
 
 	// TODO: Move this somewhere else and use secure
 	sessionCookie := &http.Cookie{
-		Name:    SessionIDCookieName,
+		Name:    sessionIDCookieName,
 		Value:   sessionID,
 		Expires: time.Now().Add(time.Duration(s.config.SessionLifetime)),
 	}
