2b98d8a42e95166d82991e826f532ac4fb7dbb37
ocboogie <boogie@mikulec.com>
2019-11-16 02:48:25 -0800

    Don't serialize passwords when encoding to json

--- models/user.go
+++ models/user.go
@@ -9,12 +9,8 @@ import (
 type User struct {
 	ID        string    `json:"id"`
 	Email     string    `json:"email"`
-	Password  string    `json:"password,omitempty"`
+	Password  string    `json:"-"`
 	CreatedAt time.Time `json:"createdAt"`
-}
-
-func (u *User) StripsSensitive() {
-	u.Password = ""
 }
 
 type UserInput struct {
