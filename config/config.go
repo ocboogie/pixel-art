9846de599624880f541ec659731719500054b28d
ocboogie <boogie@mikulec.com>
2019-12-06 04:48:17 -0800

    Add a service to validate art

--- config/config.go
+++ config/config.go
@@ -12,6 +12,9 @@ type Config struct {
 	HashConfig      *argon2.Params
 	SessionLifetime uint
 	Secret          string
+	// TODO: Should probably move this to its own struct
+	ArtSize   uint16
+	ArtColors uint8
 }
 
 func getEnvNumber(name string) (int, bool) {
@@ -35,6 +38,8 @@ func GetConfig() Config {
 		log.Fatal("You must supply a \"SECRET\" environment variable")
 	}
 	sessionLifetime := uint(7 * 24 * 60 * 60 * 1000)
+	artSize := uint16(25)
+	artColors := uint8(8)
 
 	hashConfig := argon2.DefaultParams()
 
@@ -56,10 +61,18 @@ func GetConfig() Config {
 	if val, exists := getEnvNumber("SESSION_LIFETIME"); exists {
 		sessionLifetime = uint(val)
 	}
+	if val, exists := getEnvNumber("ART_SIZE"); exists {
+		artSize = uint16(val)
+	}
+	if val, exists := getEnvNumber("ART_COLORS"); exists {
+		artColors = uint8(val)
+	}
 
 	return Config{
 		HashConfig:      hashConfig,
 		SessionLifetime: sessionLifetime,
 		Secret:          secret,
+		ArtSize:         artSize,
+		ArtColors:       artColors,
 	}
 }
---
+++ services/art/service_test.go
@@ -0,0 +1,55 @@
+package art
+
+import (
+	"encoding/base64"
+	"encoding/hex"
+	"testing"
+
+	"github.com/ocboogie/pixel-art/config"
+	"github.com/stretchr/testify/assert"
+)
+
+var cfg = &config.Config{
+	ArtSize:   3,
+	ArtColors: 3,
+}
+
+func hexToBase64(hexCode string) string {
+	bytes, err := hex.DecodeString(hexCode)
+	if err != nil {
+		panic(err)
+	}
+
+	return base64.StdEncoding.EncodeToString(bytes)
+}
+
+func TestValidate(t *testing.T) {
+	s := &service{
+		config: cfg,
+	}
+
+	t.Run("Invalid width", func(t *testing.T) {
+		assert.False(t, s.Validate(hexToBase64("01")))
+		assert.False(t, s.Validate(hexToBase64("0010")))
+	})
+	t.Run("Invalid height", func(t *testing.T) {
+		assert.False(t, s.Validate(hexToBase64("000303")))
+		assert.False(t, s.Validate(hexToBase64("00030300")))
+	})
+	t.Run("Invalid color amount", func(t *testing.T) {
+		assert.False(t, s.Validate(hexToBase64("0003000304")))
+	})
+	t.Run("Invalid color table", func(t *testing.T) {
+		assert.False(t, s.Validate(hexToBase64("0003000303FF000000")))
+	})
+	t.Run("Invalid size", func(t *testing.T) {
+		assert.False(t, s.Validate(hexToBase64("0003000303FF000000FF000000FF0001020001020001")))
+		assert.False(t, s.Validate(hexToBase64("0003000303FF000000FF000000FF00010200010200010201")))
+	})
+	t.Run("Color index out of range", func(t *testing.T) {
+		assert.False(t, s.Validate(hexToBase64("0003000303FF000000FF000000FF000102000102000103")))
+	})
+	t.Run("Valid", func(t *testing.T) {
+		assert.True(t, s.Validate(hexToBase64("0003000303FF000000FF000000FF000102000102000102")))
+	})
+}
