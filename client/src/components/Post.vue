e72646596fb7d21231ddee0dc27b1014ce24ea5b
ocboogie <boogie@mikulec.com>
2019-12-18 00:03:07 -0800

    Add a post component and add a testing page

---
+++ client/src/components/Post.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-card max-width="344" class="mx-auto post">
+    <v-list-item>
+      <v-list-item-avatar
+        class="avatar elevation-2"
+        color="white"
+      ></v-list-item-avatar>
+      <v-list-item-content>
+        <v-list-item-title>Our Changing Planetarstarstarst</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
+
+    <v-img src="https://art.pixilart.com/5c3a0d7c7f68636.png"></v-img>
+
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>mdi-share-variant</v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+// The .post is to give this a higher CSS priority, making the margin-right
+// override vuetify
+.post .avatar {
+  margin-left: -35px;
+  margin-right: 12px;
+}
+</style>
---
+++ client/src/views/Testing.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Post :post="post" />
+  </div>
+</template>
+<script>
+import Post from "../components/Post.vue";
+
+export default {
+  components: {
+    Post
+  },
+  data: () => ({
+    post: {
+      id: "122accdf-019e-4992-8889-1b0a6b025887",
+      author: {
+        id: "422cb2d3-7213-4734-aab2-5f31f0645925",
+        name: "test",
+        createdAt: "2019-11-30T00:31:22.414657Z"
+      },
+      title: "test",
+      data: "",
+      createdAt: "2019-11-30T00:34:04.207462Z"
+    }
+  })
+};
+</script>
