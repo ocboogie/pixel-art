7715f5f2f46a405275dd6c0a501ac03286dc2612
ocboogie <boogie@mikulec.com>
2020-01-17 15:09:57 -0800

    Add a base64ToArrayBuffer function

---
+++ client/src/utils/base64ToArrayBuffer.js
@@ -0,0 +1,10 @@
+//https://stackoverflow.com/a/21797381/4910911
+export default base64 => {
+  var binary_string = window.atob(base64);
+  var len = binary_string.length;
+  var bytes = new Uint8Array(len);
+  for (var i = 0; i < len; i++) {
+    bytes[i] = binary_string.charCodeAt(i);
+  }
+  return bytes.buffer;
+};
