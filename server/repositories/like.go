cd8ab729fe47f9a74385ebb1c902823a34ce5830
ocboogie <boogie@mikulec.com>
2020-01-19 12:44:08 -0800

    Remove unused import

--- server/repositories/like.go
+++ server/repositories/like.go
@@ -1,7 +1,5 @@
 package repositories
 
-import "errors"
-
 //go:generate mockgen -destination=../mocks/repo_like.go -package mocks -mock_names Like=RepositoryLike github.com/ocboogie/pixel-art/repositories Like
 
 type Like interface {
